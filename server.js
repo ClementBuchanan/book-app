c3aae1f6b80e69ba1fc39056c2f0da29bb2bbc58
ClementBuchanan <clement.buchanan@gmail.com>
2021-01-25 21:26:40 -0800

    updated server.js file

--- server.js
+++ server.js
@@ -7,56 +7,62 @@ const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
 const app = express();
-const key = BOOKS_API_KEY;
+const key = process.env.BOOKS_API_KEY;
 const url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=BOOKS_API_KEY';
 
-superagent.get(url){
-  if 
-}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
 
+app.get('/', showBooks);
 app.get('/book-search', showSearchPage);
+app.post('/book-search', makeBookSearch);
+
 app.use(cors());
 
-
-
-function Books (data) {
-  this.title = data.title ? data.title : 'Unknown Title';
-  this.author = data.author ? data.author : 'Unknown autor';
-  this.image = data.image ? data.image : 'https://i.imgur.com/J5LVHEL.jpg';
-
-
-
-}
-
 // ============ routes ==========
-app.use(express.urlencoded({extended: true}));
 
-
-app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render('pages/searches/new.ejs')
 });
+app.get('/book-search', showSearchPage);
 
-// app.get('/Hello', showHome);
+// ======= functions =============
+
+function makeBookSearch(req, res) {
+  const title = req.body.title;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${title}`;
+  superagent.get(url).then(stuff => {
+    console.log(stuff.body.items);
+    const titles = stuff.body.items.map(items =>
+      items.volumeInfo.title);
+    res.render('pages/searches/show.ejs', { titles: titles });
+  });
+}
+
+function showSearchPage(req, res) {
+  res.render('pages/searches/new.ejs');
+}
+
+function showBooks(req, res) {
+  res.render('pages/searches/new.ejs');
+}
 
 
-app.listen(PORT, () => console.log(`SERVER on ${PORT}`));
+// ======= constructors ==========
 
-
-
-// ========= server setup =========
-
-//============ Global variable ===========
-
-
-
-
-
-
-// ==========  local requests ============
-
-
-// ====== functions ===========
+function Books(data) {
+  this.title = data.title ? data.title : 'Unknown Title';
+  this.author = data.author ? data.author : 'Unknown autor';
+  if (data.image[4] === 's'){
+    const firstHalf = data.image.slice(0,3);
+    const secondHalf = data.image.slice(4);
+    data.image = `${firstHalf}s${secondHalf}`;
+  }
+  this.image = data.image ? data.image : 'https://i.imgur.com/J5LVHEL.jpg';
+}
 
 
 // ====== start server =======
+
+app.listen(PORT, () => console.log(`SERVER on ${PORT}`));
