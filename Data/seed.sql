d9139a626c3c699bf871213c44bc26569206142b
Anthony Johnson <theanthonyjohnson@gmail.com>
2021-01-28 22:05:08 -0500

    updated sql

--- Data/seed.sql
+++ Data/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO todo (task, dueDate,) VALUES ('walk', 'run', 'eat', 'drink');
-INSERT INTO todo (task, dueDate,) VALUES ('walk', 'run', 'eat', 'drink');
-INSERT INTO todo (task, dueDate,) VALUES ('walk', 'run', 'eat', 'drink');
-INSERT INTO todo (task, dueDate,) VALUES ('walk', 'run', 'eat', 'drink');
+INSERT INTO books (author, title, image_url, isbn, description) VALUES ('Mr. A', 'book a', 'https://www.dliflc.edu/wp-content/uploads/2018/11/book.jpg', '231wa2', 'nice book');
+INSERT INTO books (author, title, image_url, isbn, description) VALUES ('Mr. B', 'book b', 'https://www.dliflc.edu/wp-content/uploads/2018/11/book.jpg', '46352s', 'nice book');
+INSERT INTO books (author, title, image_url, isbn, description) VALUES ('Mr. C', 'book d', 'https://www.dliflc.edu/wp-content/uploads/2018/11/book.jpg', '323hg5', 'nice book');
+INSERT INTO books (author, title, image_url, isbn, description) VALUES ('Mr. D', 'book e', 'https://www.dliflc.edu/wp-content/uploads/2018/11/book.jpg', '645hw5', 'nice book');
